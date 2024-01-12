a0a29b92eab0dcf449ad0860f7611104007ba5a6
mardenrafael <mardenrafaeldalmagro.gimenez@gmail.com>
2024-01-12 01:14:02 -0400

    ativa body parser

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { Logger } from "nestjs-pino";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    bodyParser: false,
-  });
-  app.useLogger(app.get(Logger));
+  const app = await NestFactory.create(AppModule);
+  // app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
