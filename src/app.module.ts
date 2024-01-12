46b54a5230dba114aab8813b03c201c9ec8400d4
mardenrafael <mardenrafaeldalmagro.gimenez@gmail.com>
2024-01-11 20:43:08 -0400

    configura variaveis de ambiente e pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,27 @@
+import pino from "pino";
 import { Module } from "@nestjs/common";
 import { HealthModule } from "./health/health.module";
+import { LoggerModule } from "nestjs-pino";
+import { ConfigModule } from "@nestjs/config";
+import { LogModule } from "./log/log.module";
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        useLevel: process.env.NODE_ENV === "development" ? "debug" : "info",
+        stream: pino.destination({
+          dest: process.env.NODE_LOG_PATH || process.stdout.fd,
+          sync: true,
+        }),
+      },
+    }),
+    HealthModule,
+    LogModule,
+  ],
   controllers: [],
   providers: [],
 })
